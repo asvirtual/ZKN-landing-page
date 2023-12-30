fe87bf3b8a7c2905b6f0828330dbc0089467e6f4
Asvirtual <didack.g@gmail.com>
2023-12-30 19:10:25 +0100

    Update links in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,9 @@ import style from './About.module.css';
 
 
 const About = () => {
-    return <>
+    return <main className={ style.main }>
         <h1>
-            Founded in 2023 in Italy by industry visionaries <a href="https://www.linkedin.com/in/fabrizio-pepe/">Fabrizio Pepe</a> and <a href="https://www.linkedin.com/in/luca-polo/">Luca Polo</a>, Ziken Labs is a premium digital services agency that distinguishes itself through a commitment to innovation and proficiency. <br></br>
+            Founded in 2023 in Italy by industry visionaries <a href="https://www.linkedin.com/in/fabrizio-pepe/" target="_blank">Fabrizio Pepe</a> and <a href="https://www.linkedin.com/in/luca-polo/" target="_blank">Luca Polo</a>, Ziken Labs is a premium digital services agency that distinguishes itself through a commitment to innovation and proficiency. <br></br>
             Our core principles of delivering unparalleled quality, speed, and efficiency guide every facet of our operations. Offering a comprehensive suite of solutions, including: 
         </h1>
         <ul>
@@ -20,28 +20,28 @@ const About = () => {
             <li>Bot and Software Development</li>
         </ul>
         <h3>
-            We are the preferred partner for clients like <a href="https://trakx.io/">Trakx</a>, <a href="https://plasbit.com/">PlasBit</a>, <a href="https://www.spaziocrypto.com/">Spaziocrypto</a>, and <a href="https://evoload.co/">Evoload</a>. We are also honored to have reached over 100 orders and achieved level 2 on <a href="https://www.fiverr.com/pp_studios">our Fiverr profile</a>!<br></br>
+            We are the preferred partner for clients like <a href="https://trakx.io/" target="_blank">Trakx</a>, <a href="https://plasbit.com/" target="_blank">PlasBit</a>, <a href="https://www.spaziocrypto.com/" target="_blank">Spaziocrypto</a>, and <a href="https://evoload.co/" target="_blank">Evoload</a>. We are also honored to have reached over 100 orders and achieved level 2 on <a href="https://www.fiverr.com/pp_studios" target="_blank">our Fiverr profile</a>!<br></br>
             Join us at Ziken Labs, where we transform visionary aspirations into tangible and prosperous realities, epitomizing the pinnacle of digital success.<br></br>
             We have been featured on:<br></br>
         </h3>
         <ul>
-            <li><a href="techbullion.com">techbullion.com</a></li>
-            <li><a href="ventsmagazine.com">ventsmagazine.com</a></li>
-            <li><a href="theinscribermag.com">theinscribermag.com</a></li>
-            <li><a href="techsslash.com">techsslash.com</a></li>
-            <li><a href="entrepreneursbreak.com">entrepreneursbreak.com</a></li>
-            <li><a href="greenrecord.co.uk">greenrecord.co.uk</a></li>
-            <li><a href="metapress.com">metapress.com</a></li>
-            <li><a href="arnewsjournal.com">arnewsjournal.com</a></li>
-            <li><a href="sortlist.com">sortlist.com</a></li>
-            <li><a href="clutch.co">clutch.co</a></li>
-            <li><a href="topinteractiveagencies.com">topinteractiveagencies.com</a></li>
-            <li><a href="agencyvista.com">agencyvista.com</a></li>
-            <li><a href="designrush.com">designrush.com</a></li>
-            <li><a href="virgilio.it">virgilio.it</a></li>
-            <li><a href="paginegialle.it">paginegialle.it</a></li>
+            <li><a href="https://techbullion.com" target="_blank">techbullion.com</a></li>
+            <li><a href="https://ventsmagazine.com" target="_blank">ventsmagazine.com</a></li>
+            <li><a href="https://theinscribermag.com" target="_blank">theinscribermag.com</a></li>
+            <li><a href="https://techsslash.com" target="_blank">techsslash.com</a></li>
+            <li><a href="https://entrepreneursbreak.com" target="_blank">entrepreneursbreak.com</a></li>
+            <li><a href="https://greenrecord.co.uk" target="_blank">greenrecord.co.uk</a></li>
+            <li><a href="https://metapress.com" target="_blank">metapress.com</a></li>
+            <li><a href="https://arnewsjournal.com" target="_blank">arnewsjournal.com</a></li>
+            <li><a href="https://sortlist.com" target="_blank">sortlist.com</a></li>
+            <li><a href="https://clutch.co" target="_blank">clutch.co</a></li>
+            <li><a href="https://topinteractiveagencies.com" target="_blank">topinteractiveagencies.com</a></li>
+            <li><a href="https://agencyvista.com" target="_blank">agencyvista.com</a></li>
+            <li><a href="https://designrush.com" target="_blank">designrush.com</a></li>
+            <li><a href="https://virgilio.it" target="_blank">virgilio.it</a></li>
+            <li><a href="https://paginegialle.it" target="_blank">paginegialle.it</a></li>
         </ul>
-    </>;
+    </main>;
 };
 
 export default About;
