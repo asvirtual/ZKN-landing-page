5cea73e4829a7097771b11f8bc07663314fb8c86
Asvirtual <didack.g@gmail.com>
2024-03-06 17:48:39 +0100

    Fixed wrong meta tags with router

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -296,38 +296,6 @@ function Home() {
 
 	}, [scrollProgress])
 
-	useEffect(() => {
-		switch (window.location.pathname) {
-			case "/services": 
-				document.title = "Services | Ziken Labs"
-				document.getElementsByTagName("META")[2].content = "Discover all the services offered by Ziken Labs. Starting with your vision, we can turn your idea into reality with our experience."
-				break;
-			case "/clients": 
-				document.title = "Clients | Ziken Labs"
-				document.getElementsByTagName("META")[2].content = "Browse the projects to which Ziken Labs has already contributed. You will find detailed reports on how we can help you grow your project."
-				break;
-			case "/products": 
-				document.title = "Products | Ziken Labs"
-				document.getElementsByTagName("META")[2].content = "Discover all the products that Ziken Labs has built. Discord bots, customized GPTs, and graphic templates are just some of the products offered!"
-				break;
-			case "/about": 
-				document.title = "About | Ziken Labs"
-				document.getElementsByTagName("META")[2].content = "Find out who the team behind Ziken Labs is. You will understand how much perseverance and effort it takes to succeed in the competitive digital world."
-				break;
-			case "/blog": 
-				document.title = "Blog | Ziken Labs"
-				document.getElementsByTagName("META")[2].content = "Read all the latest publications on the Ziken Labs Blog. You will find interesting articles, insights, research, interviews and analysis."
-				break;
-			case "/contact": 
-				document.title = "Contact | Ziken Labs"
-				document.getElementsByTagName("META")[2].content = "Need a hand to grow your project? You are in the right section. Get in touch with Ziken Labs for collaborations or requests for quotes!"
-				break;
-		}
-	}, []);
-
-	if (window.location.href.includes("/about"))
-		return <About />;
-
 	const logoScale = 
 		scrollProgress <= 35 ? 1 :
 		0
